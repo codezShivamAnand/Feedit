55e4af0c90756c71d356eef59aa631810c8f8e97
Shivam Anand <proff.shivam17anand@gmail.com>
2025-05-12 21:11:21 +0530

    foodcart Options

---
+++ src/components/FoodCart.js
@@ -0,0 +1,9 @@
+export default function FoodCart({foodData}){
+    return(
+        <>
+            <a href={foodData?.action?.link}>
+                <img className="w-30 h-30 object-cover" src={"https://media-assets.swiggy.com/swiggy/image/upload/" + foodData?.imageId}></img>
+            </a>
+        </>
+    )
+}
