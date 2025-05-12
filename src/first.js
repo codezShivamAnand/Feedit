b1eb019c31adf46e126c9d8f2f2fb1d5bd1f2327
Shivam Anand <proff.shivam17anand@gmail.com>
2025-05-12 21:15:26 +0530

    FoodOption added

--- src/first.js
+++ src/first.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./components/Header"
+import Header from "./components/Header";
+import FoodOption from "./components/FoodOption";
+
 function App(){
     return(
         <>
             <Header/>
+            <FoodOption/>
         </>
     )
 }
