e649ab458cf2fd235f9256b3b4f9bffa422e924c
Shivam Anand <proff.shivam17anand@gmail.com>
2025-05-12 22:21:22 +0530

    successfully integrated Instamart option

--- src/first.js
+++ src/first.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import FoodOption from "./components/FoodOption";
+import InstamartOptions from "./components/Instamart/InstamartOptions"
 
 function App(){
     return(
         <>
             <Header/>
             <FoodOption/>
+            <InstamartOptions/>
         </>
     )
 }
