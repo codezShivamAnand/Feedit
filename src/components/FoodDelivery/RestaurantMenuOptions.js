a7640bdf631807e03233f67f87441094574418ab
Shivam Anand <proff.shivam17anand@gmail.com>
2025-05-17 21:59:43 +0530

    integrated filter options on button click

--- src/components/FoodDelivery/RestaurantMenuOptions.js
+++ src/components/FoodDelivery/RestaurantMenuOptions.js
@@ -1,12 +1,14 @@
 import RestaurantMenuOptionsDetails from "./RestaurantMenuOptionsDetails";
 
-export default function RestaurantMenuOptions({Restinfo}){
+export default function RestaurantMenuOptions({Restinfo, foodselected}){
+    
+    
     // console.log("Rendering Restinfo:", Restinfo);
     // handling case when categories is present instead of itemCards in the API 
     if( Restinfo?.categories ){  
         // "categories" in Restinfo
         return (
-            <div className=" w-[70%] mx-auto p-3 text-xl text-black font-bold">
+            <div className=" w-full mx-auto p-3 text-xl text-black font-bold">
                 <p>{Restinfo?.title}</p>
                 <div>
                     {
@@ -16,7 +18,38 @@ export default function RestaurantMenuOptions({Restinfo}){
             </div>
         )
     }
-    
+        // console.log(foodselected);
+    if(foodselected==='veg'){
+        // console.log(foodselected);
+        return (
+            <div className="w-[70%] mx-auto p-3">
+                <div className="text-xl text-black font-bold">
+                    <p>{Restinfo?.title}</p>
+                </div>
+                <div className="w-full mt-3 ">
+                    {
+                        Restinfo?.itemCards?.filter((food)=>"isVeg" in food?.card?.info)?.map((items)=><RestaurantMenuOptionsDetails key={items?.card?.info?.id} items={items?.card?.info} foodselected={foodselected}/>)
+                    }
+                </div>
+            </div> 
+        )
+    }
+
+    if(foodselected==='nonveg'){
+        return (
+            <div className="w-[70%] mx-auto p-3">
+                <div className="text-xl text-black font-bold">
+                    <p>{Restinfo?.title}</p>
+                </div>
+                <div className="w-full mt-3 ">
+                    {
+                        Restinfo?.itemCards?.filter((food)=>food?.card?.info?.isVeg != 1)?.map((items)=><RestaurantMenuOptionsDetails key={items?.card?.info?.id} items={items?.card?.info} foodselected={foodselected}/>)
+                    }
+                </div>
+            </div> 
+        )
+    }
+
     return(
         <>
         <div className="w-[70%] mx-auto p-3">
@@ -25,7 +58,7 @@ export default function RestaurantMenuOptions({Restinfo}){
                 </div>
                 <div className="w-full mt-3 ">
                     {
-                        Restinfo?.itemCards?.map((items)=><RestaurantMenuOptionsDetails key={items?.card?.info?.id} items={items?.card?.info} />)
+                        Restinfo?.itemCards?.map((items)=><RestaurantMenuOptionsDetails key={items?.card?.info?.id} items={items?.card?.info}  foodselected={foodselected}/>)
                     }
                 </div>
         </div>
