a004698eef220516d964266113cad18e9fc4177f
Shivam Anand <proff.shivam17anand@gmail.com>
2025-05-18 16:43:17 +0530

    added routing for searching food

--- src/first.js
+++ src/first.js
@@ -4,15 +4,19 @@ import Restaurant from "./components/FoodDelivery/Restaurant";
 import {BrowserRouter, Routes, Route} from "react-router";
 import Home from "./pages/Home"
 import RestaurantMenu from "./components/FoodDelivery/RestaurantMenu";
+import Searchfood from "./components/FoodDelivery/Searchfood";
+import Instamart1 from "./pages/InstamartPage/Instamart1";
 
 function App(){
     return(
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Home/>}></Route>
-                    <Route path="/restaurant" element={<Restaurant/>}></Route>
-                    <Route path="/city/delhi/:id" element={<RestaurantMenu/>}></Route>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/restaurant" element={<Restaurant/>} />
+                    <Route path="/city/delhi/:id" element={<RestaurantMenu/>} />
+                    <Route path="/instamart" element={<Instamart1 />} />
+                    <Route path="/city/delhi/:id/search" element={<Searchfood />} />
                 </Routes>
             </BrowserRouter>
         </>
