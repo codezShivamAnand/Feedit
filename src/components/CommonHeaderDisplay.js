7797119c3d4608d9d85a7735ab7b458ce8704033
Shivam Anand <proff.shivam17anand@gmail.com>
2025-05-19 23:59:00 +0530

    common header for all pages which displays items in cart

--- src/components/CommonHeaderDisplay.js
+++ src/components/CommonHeaderDisplay.js
@@ -1,8 +1,15 @@
+import { useSelector } from "react-redux"
+import { Link } from "react-router";
+
 export default function CommonHeaderDisplay(){
+    
+    const counter = useSelector(state=> state.cartslice.count);
     return(
         <div className="w-[100%] mx-auto bg-gray-300 flex justify-between items-center py-3">
             <div className=" mx-auto font-bold text-shadow-md text-green-400 text-2xl p-2">Feedit</div>
-            <div className=" mx-auto font-semibold text-gray-600 text-xl p-2">Cart</div>
+            <Link to="/Checkout">
+                <div className=" mx-auto font-semibold text-gray-600 text-xl p-2">Cart {`(${counter})`}</div>
+            </Link>
         </div>
     )
 }
